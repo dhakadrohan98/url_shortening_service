06188e7755cf152ee0a3e9cc69927a425ae30d99
dhakadrohan98 <rohandhakad20@gmail.com>
2024-09-11 13:04:54 +0530

    registered url shortening application as eureka client with service registry & ran multiple instances of this service

--- src/main/java/com/wissen/url_shortening/UrlShorteningApplication.java
+++ src/main/java/com/wissen/url_shortening/UrlShorteningApplication.java
@@ -1,13 +1,75 @@
 package com.wissen.url_shortening;
 
+import com.wissen.url_shortening.pojo.UrlShorteningRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.TimeUnit;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UrlShorteningApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UrlShorteningApplication.class, args);
-	}
+//    @Autowired
+//    private RestTemplate restTemplate;
 
+    public static void main(String[] args) {
+        SpringApplication.run(UrlShorteningApplication.class, args);
+    }
+
+//    @Override
+//    public void run(String... args) throws Exception {
+//        //create a thread pool executor with fixed no. of threads
+//        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+//        taskExecutor.setCorePoolSize(400); //no. of threads to use
+//        taskExecutor.setMaxPoolSize(500); //max threads
+//        taskExecutor.setQueueCapacity(1000); //queue capacity for tasks
+//        taskExecutor.initialize();
+//
+//        String baseUrl = "http://localhost:8081/generate"; //API url
+//
+//        for (int i = 1000; i < 2000; i++) {
+//            Thread.sleep(50); // wait for 50 ms
+//            final int index = i;
+//            taskExecutor.submit(() -> {
+//                String originalUrl = "https://github.com/dhakadrohan98/morgan_stanley/blob/dsa/Goldman_Sachs/src/com/wissen/DP/AllSubsequencesOfString.java";
+//                String expirationDate = "2024-09-30T23:10:00";
+//                try {
+//                    String shortUrl = callUrlShorteningApi(baseUrl, originalUrl, expirationDate);
+//                    // Log or save the response if necessary
+//                    System.out.println("Shortened URL: " + shortUrl);
+//                } catch (Exception e) {
+//                    System.err.println("Failed to shorten URL: " + e.getMessage());
+//                }
+//            });
+//        }
+//        // Shutdown the executor after all tasks are completed
+//        taskExecutor.shutdown();
+//        taskExecutor.getThreadPoolExecutor().awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+//    }
+//
+//    private String callUrlShorteningApi(String baseUrl, String originalUrl, String expirationDate) {
+//        // create a new UrlShorteningRequest object for each request
+//        UrlShorteningRequest urlShorteningRequest = new UrlShorteningRequest(originalUrl, expirationDate);
+//
+//        //set the request headers
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setContentType(MediaType.APPLICATION_JSON);
+//
+//        //create the HttpEntity object with the request body & headers
+//        HttpEntity<UrlShorteningRequest> entity = new HttpEntity<>(urlShorteningRequest, headers);
+//
+//        // Make the POST request to the API
+//        return restTemplate.exchange(baseUrl, HttpMethod.POST, entity, String.class).getBody();
+//    }
 }
--- src/main/java/com/wissen/url_shortening/controller/UrlShorteningController.java
+++ src/main/java/com/wissen/url_shortening/controller/UrlShorteningController.java
@@ -1,92 +1,92 @@
-package com.wissen.url_shortening.controller;
+    package com.wissen.url_shortening.controller;
 
-import com.wissen.url_shortening.model.Url;
-import com.wissen.url_shortening.model.UrlDto;
-import com.wissen.url_shortening.model.UrlErrorResponseDto;
-import com.wissen.url_shortening.model.UrlResponseDto;
-import com.wissen.url_shortening.service.UrlService;
-import io.micrometer.common.util.StringUtils;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+    import com.wissen.url_shortening.model.Url;
+    import com.wissen.url_shortening.model.UrlDto;
+    import com.wissen.url_shortening.model.UrlErrorResponseDto;
+    import com.wissen.url_shortening.model.UrlResponseDto;
+    import com.wissen.url_shortening.service.UrlService;
+    import io.micrometer.common.util.StringUtils;
+    import jakarta.servlet.http.HttpServletResponse;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.http.HttpStatus;
+    import org.springframework.http.ResponseEntity;
+    import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
-import java.time.LocalDateTime;
+    import java.io.IOException;
+    import java.time.LocalDateTime;
 
-@RestController
-public class UrlShorteningController {
+    @RestController
+    public class UrlShorteningController {
 
-    @Autowired
-    private UrlService urlService;
-    @Autowired
-    private UrlResponseDto urlResponseDto;
-    @Autowired
-    private UrlErrorResponseDto urlErrorResponseDto;
+        @Autowired
+        private UrlService urlService;
+        @Autowired
+        private UrlResponseDto urlResponseDto;
+        @Autowired
+        private UrlErrorResponseDto urlErrorResponseDto;
 
-    @PostMapping("/generate")
-    public ResponseEntity<?> generateShortLink(@RequestBody UrlDto urlDto) {
-        Url urlToRet = urlService.generateShortLink(urlDto);
-        //if url object(not null) that need to prepare(according to some format), then return UrlReponse object
-        if(urlToRet != null) {
-            urlResponseDto.setOriginalUrl(urlToRet.getOriginalUrl());
-            urlResponseDto.setExpirationDate(urlToRet.getExpirationDate());
-            urlResponseDto.setShortLink(urlToRet.getShortLink());
-            return new ResponseEntity<UrlResponseDto>(urlResponseDto, HttpStatus.OK);
-        }
-        //If we are not able to generate the short link, So prepare url Error Reponse object & return it
-        urlErrorResponseDto.setStatus("404");
-        urlErrorResponseDto.setError("There was an error processing your request, " +
-                "please try again");
-        return new ResponseEntity<UrlErrorResponseDto>(urlErrorResponseDto, HttpStatus.OK);
-    }
-
-    @GetMapping("/{shortLink}")
-    public ResponseEntity<?> redirectToOriginalUrl(@PathVariable String shortLink, HttpServletResponse response) {
-        //if passed short link is empty
-        if(StringUtils.isEmpty(shortLink)) {
+        @PostMapping("/generate")
+        public ResponseEntity<?> generateShortLink(@RequestBody UrlDto urlDto) {
+            Url urlToRet = urlService.generateShortLink(urlDto);
+            //if url object(not null) that need to prepare(according to some format), then return UrlReponse object
+            if(urlToRet != null) {
+                urlResponseDto.setOriginalUrl(urlToRet.getOriginalUrl());
+                urlResponseDto.setExpirationDate(urlToRet.getExpirationDate());
+                urlResponseDto.setShortLink(urlToRet.getShortLink());
+                return new ResponseEntity<UrlResponseDto>(urlResponseDto, HttpStatus.OK);
+            }
+            //If we are not able to generate the short link, So prepare url Error Reponse object & return it
             urlErrorResponseDto.setStatus("404");
-            urlErrorResponseDto.setError("Invalid url");
+            urlErrorResponseDto.setError("There was an error processing your request, " +
+                    "please try again");
             return new ResponseEntity<UrlErrorResponseDto>(urlErrorResponseDto, HttpStatus.OK);
         }
 
-        Url urlToReturn = urlService.getEncodedUrl(shortLink);
-        //if url is expired does not exist due to some other reason
-        if(urlToReturn == null) {
-            urlErrorResponseDto.setError("url does not exist or it might have been expired");
-            urlErrorResponseDto.setStatus("400");
-            return new ResponseEntity<UrlErrorResponseDto>(urlErrorResponseDto, HttpStatus.OK);
-        }
-        //if url has been expired
-        if(urlToReturn.getExpirationDate().isBefore(LocalDateTime.now())) {
-            //delete the url because it is expired
-            urlService.deleteShortLink(urlToReturn);
-            //set response in urlErrorReponseDto object & return this response
-            urlErrorResponseDto.setError("Url has been expired. please generate a new one");
-            urlErrorResponseDto.setStatus("200");
-            return new ResponseEntity<UrlErrorResponseDto>(urlErrorResponseDto, HttpStatus.OK);
+        @GetMapping("/{shortLink}")
+        public ResponseEntity<?> redirectToOriginalUrl(@PathVariable String shortLink, HttpServletResponse response) {
+            //if passed short link is empty
+            if(StringUtils.isEmpty(shortLink)) {
+                urlErrorResponseDto.setStatus("404");
+                urlErrorResponseDto.setError("Invalid url");
+                return new ResponseEntity<UrlErrorResponseDto>(urlErrorResponseDto, HttpStatus.OK);
+            }
+
+            Url urlToReturn = urlService.getEncodedUrl(shortLink);
+            //if url is expired does not exist due to some other reason
+            if(urlToReturn == null) {
+                urlErrorResponseDto.setError("url does not exist or it might have been expired");
+                urlErrorResponseDto.setStatus("400");
+                return new ResponseEntity<UrlErrorResponseDto>(urlErrorResponseDto, HttpStatus.OK);
+            }
+            //if url has been expired
+            if(urlToReturn.getExpirationDate().isBefore(LocalDateTime.now())) {
+                //delete the url because it is expired
+                urlService.deleteShortLink(urlToReturn);
+                //set response in urlErrorReponseDto object & return this response
+                urlErrorResponseDto.setError("Url has been expired. please generate a new one");
+                urlErrorResponseDto.setStatus("200");
+                return new ResponseEntity<UrlErrorResponseDto>(urlErrorResponseDto, HttpStatus.OK);
+            }
+
+            // Redirect to the original URL, if url is not expired
+            try {
+                //Sends a redirect response to the client using the specified redirect
+                // location URL with the status code SC_FOUND 302 (Found), clears the
+                // response buffer and commits the response
+                response.sendRedirect(urlToReturn.getOriginalUrl());
+            } catch (IOException e) {
+                urlErrorResponseDto.setStatus("500");
+                urlErrorResponseDto.setError("Error during redirection");
+                return new ResponseEntity<UrlErrorResponseDto>(urlErrorResponseDto, HttpStatus.OK);
+            }
+
+            return null; // This line will never be reached
         }
 
-        // Redirect to the original URL, if url is not expired
-        try {
-            //Sends a redirect response to the client using the specified redirect
-            // location URL with the status code SC_FOUND 302 (Found), clears the
-            // response buffer and commits the response
-            response.sendRedirect(urlToReturn.getOriginalUrl());
-        } catch (IOException e) {
-            urlErrorResponseDto.setStatus("500");
-            urlErrorResponseDto.setError("Error during redirection");
-            return new ResponseEntity<UrlErrorResponseDto>(urlErrorResponseDto, HttpStatus.OK);
+        @DeleteMapping("/{shortLink}")
+        public void deleteShortUrl(@PathVariable String shortLink) {
+            Url urlObj = urlService.getEncodedUrl(shortLink);
+            urlService.deleteShortLink(urlObj);
         }
 
-        return null; // This line will never be reached
     }
-
-    @DeleteMapping("/{shortLink}")
-    public void deleteShortUrl(@PathVariable String shortLink) {
-        Url urlObj = urlService.getEncodedUrl(shortLink);
-        urlService.deleteShortLink(urlObj);
-    }
-
-}
